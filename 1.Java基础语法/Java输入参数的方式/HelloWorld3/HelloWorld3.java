e4f6e425e3be3660dcbaf848850bed942fe90b58
zengxiaowei <zeng9702@gmail.com>
2017-03-15 13:57:54 +0800

    第一个Java程序
    HelloWorld

---
+++ 1.Java基础语法/Java输入参数的方式/HelloWorld3/HelloWorld3.java
@@ -0,0 +1,12 @@
+/*
+*2017.3.15
+*在main方法内直接设置参数
+*/
+public class HelloWorld3{
+	public static void main(String args[]){
+		String array [] = {"Hello","World","!"};
+		for(int i = 0;i < array.length;i++){
+			System.out.print(array[i]);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# LearnJavaFromScratch
-My Java Learning Record.
+#我的Java学习记录#
+ 
+## 1.Java基础语法 ##
+**1.1 第一个Java程序**.
+
+-	[HelloWorld](https://github.com/Zeng9702/LearnJavaFromScratch/blob/master/HelloWorld/HelloWorld.java)
+
+**1.2 Java输入参数的方式**
